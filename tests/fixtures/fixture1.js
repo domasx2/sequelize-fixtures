7069ac1a54b54d98e537dd910e857b49dc18bdc2
Tyler Keating <tyler@7x7software.com>
2014-08-13 11:21:54 -0600

    Adds the ability to load JavaScript files.
    JS files are useful for fixtures as they allow you to define dynamic data for your records. For example, `createdAt: new Date()` or `password: bcrypt.hashSync("B4c0/\/", salt)`. Includes unit tests to verify the file path lookup works.

---
+++ tests/fixtures/fixture1.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        "model":"Foo",
+        "data": {
+            "propA": "tralivali",
+            "propB": 2 + 2
+        }
+    },
+    {
+        "model":"Foo",
+        "data": {
+            "propA": "treerre",
+            "propB": 4 / 2
+        }
+    },
+    {
+        "model":"Bar",
+        "data": {
+            "propA": (new Date()).toString(),
+            "propB": 43
+        }
+    }
+];
