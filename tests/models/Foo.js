90f5d913decf037616b15c10f41c411f853d27a8
Domas Lapinskas <domasx2@gmail.com>
2014-11-07 11:36:19 +0200

    test fix, version bump

--- tests/models/Foo.js
+++ tests/models/Foo.js
@@ -1,7 +1,7 @@
 module.exports = function (sequelize, DataTypes) {
     return sequelize.define("foo", {
         propA: {type: DataTypes.STRING},
-        propB: {type: DataTypes.INTEGER}
+        propB: {type: DataTypes.INTEGER},
         status: {type: DataTypes.BOOLEAN}
     });
 };
