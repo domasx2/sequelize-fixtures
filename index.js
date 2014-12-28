ba5f3165451bf7ae3e30d8bc05dc98582a71d92f
ekmartin <ekmartinb@gmail.com>
2014-12-28 01:36:30 +0100

    Rewrite the readers to use Bluebird Promises and update the exposed index-functions to return promises

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var Loader = require('./lib/loader'),
-    Reader = require('./lib/reader');
+var Loader  = require('./lib/loader'),
+    Reader  = require('./lib/reader');
 
 function initopts(options){
     options = options || {};
@@ -8,46 +8,41 @@ function initopts(options){
     return options;
 }
 
-function wrap(fn){
-    return function(){
-        if(arguments.length < 2){
-            throw new error('Insufficient arguments');
+function wrap(fn) {
+    return function() {
+        if (arguments.length < 2) {
+            throw new Error('Insufficient arguments');
         } else {
             var fixtures = arguments[0], models = arguments[1], options, cb, i;
-            for(i=2;i<arguments.length;i++){
-                if(typeof arguments[i] == 'object') options = arguments[i];
-                else if (typeof arguments[i] == 'function') cb = arguments[i];
+            for(i = 2; i < arguments.length; i++) {
+                if (typeof arguments[i] === 'object') options = arguments[i];
+                else if (typeof arguments[i] === 'function') cb = arguments[i];
             }
             return fn(fixtures, models, initopts(options), cb);
         }
     };
 }
 
-
-exports.loadFixture = wrap(function (fixture, models, options, cb) {
+exports.loadFixture = wrap(function(fixture, models, options, cb) {
     var loader = new Loader(options);
-    loader.loadFixture(fixture, models, cb);
-    return loader;
+    return loader.loadFixture(fixture, models, cb);
 });
 
-exports.loadFixtures = wrap(function (fixtures, models, options, cb) {
+exports.loadFixtures = wrap(function(fixtures, models, options, cb) {
     var loader = new Loader(options);
-    loader.loadFixtures(fixtures, models,  cb);
-    return loader;
+    return loader.loadFixtures(fixtures, models,  cb);
 });
 
-exports.loadFile = wrap(function (filename, models, options, cb) {
+exports.loadFile = wrap(function(filename, models, options, cb) {
     var loader = new Loader(options), reader = new Reader(options);
-    reader.readFileGlob(filename, function(fixtures){
-        loader.loadFixtures(fixtures, models, cb);
+    return reader.readFileGlob(filename).then(function(fixtures) {
+        return loader.loadFixtures(fixtures, models, cb);
     });
-    return loader;
 });
 
-exports.loadFiles = wrap(function (filenames, models, options, cb) {
+exports.loadFiles = wrap(function(filenames, models, options, cb) {
     var loader = new Loader(options), reader = new Reader(options);
-    reader.readFiles(filenames, function(fixtures){
-        loader.loadFixtures(fixtures, models, cb);
+    return reader.readFiles(filenames).then(function(fixtures){
+        return loader.loadFixtures(fixtures, models, cb);
     });
-    return loader;
 });
