d33a3f0e1d74e2afea4e680f3fe4cf48f6344bfa
Domas Lapinskas <domasx2@gmail.com>
2015-02-22 11:39:38 +0200

    fix tests

--- tests/models/Article.js
+++ tests/models/Article.js
@@ -1,9 +1,7 @@
 module.exports = function (sequelize, DataTypes) {
     return sequelize.define("article", {
         title: {type: DataTypes.STRING},
-        slug: {type: DataTypes.STRING, set: function (slug) {
-            return this.setDataValue('slug', slug.replace(/[^a-z0-9_-]/, ''));
-        }},
+        slug: {type: DataTypes.STRING},
         body: {type: DataTypes.TEXT}
     });
 };
