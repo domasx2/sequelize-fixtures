74ecf4fd8d64cd54d47f90bb2ce7152847503755
Domas Lapinskas <domasx2@gmail.com>
2015-05-20 22:22:05 +0300

    deprecate callbacks

--- index.js
+++ index.js
@@ -13,36 +13,47 @@ function wrap(fn) {
         if (arguments.length < 2) {
             throw new Error('Insufficient arguments');
         } else {
-            var fixtures = arguments[0], models = arguments[1], options, cb, i;
+            var fixtures = arguments[0], models = arguments[1], options, i, cb, promise;
             for(i = 2; i < arguments.length; i++) {
                 if (typeof arguments[i] === 'object') options = arguments[i];
                 else if (typeof arguments[i] === 'function') cb = arguments[i];
             }
-            return fn(fixtures, models, initopts(options), cb);
+            promise = fn(fixtures, models, initopts(options));
+            if (cb) {
+                console.warn("Sequelize-fixtures: callback arguments are deprecated, please use returned promises.");
+                promise.then(function () {
+                    cb();
+                }, function (err) {
+                    cb(err);
+                }).catch(function (err){
+                    cb(err);
+                });
+            }
+            return promise;
         }
     };
 }
 
-exports.loadFixture = wrap(function(fixture, models, options, cb) {
+exports.loadFixture = wrap(function(fixture, models, options) {
     var loader = new Loader(options);
-    return loader.loadFixture(fixture, models, cb);
+    return loader.loadFixture(fixture, models);
 });
 
-exports.loadFixtures = wrap(function(fixtures, models, options, cb) {
+exports.loadFixtures = wrap(function(fixtures, models, options) {
     var loader = new Loader(options);
-    return loader.loadFixtures(fixtures, models,  cb);
+    return loader.loadFixtures(fixtures, models);
 });
 
-exports.loadFile = wrap(function(filename, models, options, cb) {
+exports.loadFile = wrap(function(filename, models, options) {
     var loader = new Loader(options), reader = new Reader(options);
     return reader.readFileGlob(filename).then(function(fixtures) {
-        return loader.loadFixtures(fixtures, models, cb);
+        return loader.loadFixtures(fixtures, models);
     });
 });
 
-exports.loadFiles = wrap(function(filenames, models, options, cb) {
+exports.loadFiles = wrap(function(filenames, models, options) {
     var loader = new Loader(options), reader = new Reader(options);
     return reader.readFiles(filenames).then(function(fixtures){
-        return loader.loadFixtures(fixtures, models, cb);
+        return loader.loadFixtures(fixtures, models);
     });
 });
