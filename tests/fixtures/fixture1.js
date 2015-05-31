66b3b7f2d6eaa99a9c9dbc65b76ac71023e29d5b
Domas Lapinskas <domasx2@gmail.com>
2015-05-31 13:55:44 +0300

    warn about unknown fields

--- tests/fixtures/fixture1.js
+++ tests/fixtures/fixture1.js
@@ -3,24 +3,21 @@ module.exports = [
         "model":"Foo",
         "data": {
             "propA": "tralivali",
-            "propB": 2 + 2,
-            "status": false
+            "propB": 2 + 2
         }
     },
     {
         "model":"Foo",
         "data": {
             "propA": "treerre",
-            "propB": 4 / 2,
-            "status": false
+            "propB": 4 / 2
         }
     },
     {
         "model":"Bar",
         "data": {
             "propA": (new Date()).toString(),
-            "propB": 43,
-            "status": false
+            "propB": 43
         }
     }
 ];
