a0f815a2cb46aba75bc431f72f10bae70cfb81a2
Domas Lapinskas <domasx2@gmail.com>
2015-07-17 17:25:33 +0300

    fix for older sequelizes, grunt task

--- tasks/fixtures.js
+++ tasks/fixtures.js
@@ -7,20 +7,22 @@ module.exports = function(grunt) {
             done = this.async(),
             options = data.options  || {},
             loader;
-        if(typeof models == 'string') models = require(models);
-        else if(models.call) models = models();
 
-        var callback = function () {
-            console.log(loader.saved+' fixtures loaded.');
-            done();
-        };
+        if (typeof models == 'string') {
+            models = require(models);
+        } else if (models.call) {
+            models = models();
+        }
 
         var sources = [];
         this.files.forEach(function(f){
             [].push.apply(sources, f.src);
         });
-        if(sources.length){
-            loader = sf.loadFiles(sources, models, options, callback);
+        if (sources.length) {
+            sf.loadFiles(sources, models, options).then(function(saved){
+                console.log(saved+' fixtures loaded.');
+                done();
+            });
         } else {
             throw new Error('no sources provided');
         }
